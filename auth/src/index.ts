92ed0994c96ada6dca94144d0b4b1a8657ab39ce
unknown <leejs@am.local>
2021-11-08 18:02:16 +0900

    add auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 const app = express();
 app.use(express.json());
 
-app.get('/api/users/currentuser', (req,res) => {
-    res.send('who are you');
-});
-
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.set('port',3000);
 app.listen(app.get('port'), () => {
