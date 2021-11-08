e04de49e551b4d7f047859d220b819ded37bad23
leejs <75758585@naver.com>
2021-11-08 23:14:06 +0900

    add errorhandler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,8 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+app.use(errorHandler);
+
 
 app.set('port',3000);
 app.listen(app.get('port'), () => {
