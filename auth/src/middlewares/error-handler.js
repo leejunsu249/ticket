1aab4424e24cdd54c5a2fa4a556ff4072da3abcb
leejs <75758585@naver.com>
2021-11-12 21:42:30 +0900

    add mongo depl

---
+++ auth/src/middlewares/error-handler.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.errorHandler = void 0;
+var customError_1 = require("../errors/customError");
+var errorHandler = function (err, req, res, next) {
+    if (err instanceof customError_1.CustomError) {
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+    }
+    res.status(400).send({
+        errors: [{ message: 'Something wrong' }]
+    });
+};
+exports.errorHandler = errorHandler;
---
+++ auth/src/routes/signout.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.signoutRouter = void 0;
+var express_1 = require("express");
+var router = (0, express_1.Router)();
+exports.signoutRouter = router;
+router.post('/api/users/signout', function (req, res) {
+});
