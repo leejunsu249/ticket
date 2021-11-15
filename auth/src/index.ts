77b7159e11516309b80170b3ec69a5e7f045dd92
leejs <75758585@naver.com>
2021-11-15 22:09:03 +0900

    add cookiesession

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 
 import { currentUserRouter } from './routes/current-user';
@@ -11,7 +12,14 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(express.json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true,
+    })
+);
 
 app.get('/index',(req,res) => {
     res.send('hello');
